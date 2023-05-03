b88dcb708baeb7a71d8f551614cf29470322021c
awaized1 <aduolaayomide@gmail.com>
2023-05-02 20:47:40 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
 	listint_t *current = NULL;
-	unsigned int c = 0;
+	unsigned int i = 0;
 
 	if (*head == NULL)
 		return (-1);
@@ -23,12 +23,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (c < index - 1)
+	while (i < index - 1)
 	{
 		if (!temp || !(temp->next))
 			return (-1);
 		temp = temp->next;
-		c++;
+		i++;
 	}
 
 
