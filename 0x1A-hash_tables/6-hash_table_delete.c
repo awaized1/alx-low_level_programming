80cd810173115af5f32c031bc9265157f5b3d663
awaized1 <aduolaayomide@gmail.com>
2023-10-07 02:09:48 +0000

    6-hash_table_delete.c corrected

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,7 +11,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *node, *tmp;
 	unsigned long int n;
 
-	for (nm = 0; n < ht->size; n++)
+	for (n = 0; n < ht->size; n++)
 	{
 		if (ht->array[n] != NULL)
 		{
